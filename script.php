3969ac1f723b9daf4861f2e89737a85d64a8564a
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-18 21:50:48 +0100

    added sript element

--- script.php
+++ script.php
@@ -1,6 +1,46 @@
 <?php
 
-$text = $_GET['text'];
-$censored = $_GET['censored'];
+$text = trim($_GET['text']);
+$censored = trim($_GET['censored']);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form PHP</title>
+
+    
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <main>
+        <section id='form-php'>
+            <div class="container">
+                <h1 class='text-center my-5'>Form php</h1>
+                <div class="form-text">
+                    <h3>Testo Form</h3>
+                    <p><?= $text?></p>
+                    <h5>Lunghezza testo</h5>
+                    <p><?= strlen($text) ?></p>
+                </div>
+                <div class="form-censored">
+                    <h4>Testo da censurare</h4>
+                    <p><?= $censored ?></p>
+                </div>
+                <div class="text-censored">
+                    <h3>Testo censurato</h3>
+                    <p><?php $text_censored = str_replace($censored, '***', $text);
+                    echo $text_censored;
+                    ?></p>
+                    <p><?= strlen($text_censored) ?></p>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
